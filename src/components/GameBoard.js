0526ec0a139eddcca4fc5c8cdca6f9db667b3500
mallorysmith64 <mallorysmith64@gmail.com>
2019-12-16 07:01:26 -0500

    no issues with this project, github commits however are NOT being counted for any projects for several weeks now just noticed this issue, working on solution

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import Cell from './Cell'
 import WinningLogic from './WinningLogic'
 
-//testing: commits not being counted for weeks now
-
 const GameBoard = () => {
   const [cells, setCells] = useState(Array(9).fill(null))
   const [XNext, setXNext] = useState(true)
